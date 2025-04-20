5daaef8e37bd19993966fdb54c4ff3c75ecac2ce
Salmaait1 <151551142+Salmaait1@users.noreply.github.com>
2025-04-20 22:17:42 +0200

    Update server.c

--- server.c
+++ server.c
@@ -49,7 +49,7 @@ void melangerDeck()
 
 void createTable()
 {
-    // Chaque joueur reçoit 3 cartes, la carte coupable est le 13ème
+    // Chaque joueur reçoit 3 cartes
     int i, j, c;
     for (i = 0; i < 4; i++)
         for (j = 0; j < 8; j++)
